9925539680f14ad6d39ec6d015e11d7b8a9c4f5a
tinhlv44 <98213200+tinhlv44@users.noreply.github.com>
2025-01-19 11:47:50 +0700

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const app = express();
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -14,17 +15,17 @@ app.use("/api/products", productRoute);
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
-
+// Sử dụng các biến từ .env
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 mongoose
-  .connect(
-    "mongodb+srv://tinh:tinh@cluster0.1kx76.mongodb.net/bartender?retryWrites=true&w=majority"
-  )
+  .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server is running on port 3000");
     });
   })
-  .catch(() => {
-    console.log("Connection failed!");
+  .catch((e) => {
+    console.log("Connection failed!" + e);
   });
